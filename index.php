03b583d0a7a46af10ff27f5b379b965ad09c5c94
JobYegor <alohawind@mail.ru>
2016-09-27 21:10:26 +0300

    dont wokr test in upload.php

--- index.php
+++ index.php
@@ -20,32 +20,47 @@ if(!isset($_SESSION['name']) || !isset($_SESSION['count']))
 <p>Счетчик:<?=$_SESSION['count']?></p>
 <p style="margin-bottom: 20px"><a href="logout.php">Выход</a></p>
 <form action="php/upload.php" class="forma" metod="POST" enctype="multipart/form-data">
-	<div class="forma__name">Имя:<input type="text" class="forma__name__field"></div>
-	<div class="forma__email">Email:<input type="text" class="forma__email__field"></div>
-	<div class="forma__photo"><input type="file" class="forma__photo__button"></div>
-	<div class="forma__submit"><input type="submit" class="forma__submit__button" value="Отправить"></div>
+	<div class="forma__name">Имя:<input type="text" name="firstname" class="forma__name__field"></div>
+	<div class="forma__email">Email:<input type="text" name="email" class="forma__email__field"></div>
+	<div class="forma__photo"><input type="file" name="photo" class="forma__photo__button"></div>
+	<div class="forma__submit"><input type="submit" name="submit" class="forma__submit__button" value="Отправить"></div>
 </form>
+<div class="result"></div>
 <script src="js/jquery-2.1.4.js"></script>
 <script>
 $(function() {
 	$('.forma__submit__button').click(function(e) {
-		e.preventDefault();
+		// e.preventDefault();
+		var dataform = {};
+		var result = '';
+		var fields = $('.forma [name="firstname"], .forma [name="email"], .forma [name="photo"]');
+		fields.each(function(ndx, elem) {
+			dataform[$(elem).attr('name')] = $(elem).val();
+			result += '<div>'+$(elem).attr('name')+' : '+$(elem).val()+'</div>';
+		});
+		// console.log(dataform);
+		// $('.result').html(result);
+
+		var data = JSON.stringify(dataform);
+		console.log(data);
+
 		$.ajax({
 			url : 'php/upload.php',
-			method : 'post',
+			type : 'post',
 			data : {
-				data : dataform
+				data : data
 				},
 			success : function(result) {
-
+				$('.result').html( $('.result').html()+'<div>Все ушло</div>' )
 			},
 			error : function(err) {
-				console.log('Ошибка');
+				console.log(err.message);
 
-			}
-			}
+			},
+			dataType: 'json'
 		});
-	})
+	return false;	
+	});
 });
 </script>
 </body>
--- php/upload.php
+++ php/upload.php
@@ -1,5 +1,9 @@
-<?
-// echo 'OLOLOOOLL';
-if(empty($_POST) || empty($_FILES))
-	exit ('Нет данных');
-exit ('Все хорошо');
+<?php
+// session_start();
+if(empty($_POST) || empty($_FILES)) {
+	print_r $_POST;
+	print_r $_FILES;
+	exit("Нету данных");
+}
+exit('Данные пришли');
+?>
