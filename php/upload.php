29bf2d4394bfbdac942ce00270a47202e683ad3b
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2016-09-29 10:11:28 +0300

    add test type image

--- php/upload.php
+++ php/upload.php
@@ -24,11 +24,26 @@ $type = strtolower(strchr($file['name'], '.'));
 
 // создаем уникальное иимя
 $filename = md5(uniqid(rand(10000,99999)));
+$filename_copy = $filename.'_copy';
 
 //  собираем путь до папки, в которую собираемся положить файл
 // !!! $_SERVER['DOCUMENT_ROOT'] ведет только папки htdocs
 // надо править путь в файле httpd.conf  до нужного места
-
 $file_dist = $_SERVER['DOCUMENT_ROOT'].'/galleryphp/files/'.$filename.$type;
-echo $file_dist;
+$file_dist_copy = $_SERVER['DOCUMENT_ROOT'].'/galleryphp/files/'.$filename.'_copy'.$type;
+
+// проверка по расширению
+if($file['type'] != 'image/jpeg' && $file['type'] != 'image/gif' && $file['type'] != 'image/png')
+	exit('Не верный тип файла');
+
+// проверка типа файла через mime 
+$imageinfo = getimagesize($file['tmp_name']);
+if($imageinfo['mime'] != 'image/jpeg' && $imageinfo['mime'] != 'image/gif' && $imageinfo['mime'] != 'image/png')
+	exit('Не верный тип файла');
+
+// перемещаем файл из tmp в папку
+if(!move_uploaded_file($file['tmp_name'], $file_dist))
+	echo 'Не переместили файл:'.$filename;
+echo 'Переместили файл:'.$filename;
+
 ?>
